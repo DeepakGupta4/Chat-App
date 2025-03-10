f36fd392f1a23e3a0088c902d30004779f888f42
DeepakGupta4 <dkguptak9369.com>
2025-03-10 09:46:33 +0530

    Updated conversation API and fixed token error

--- DataBase/db.js
+++ DataBase/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect("mongodb://localhost:27017/chatApp").then((response)=>{
+
+mongoose.connect(process.env.MONGO_DB_URI).then((response)=>{
     console.log("DB Connected Successfully")
 }).catch(err=>{
     console.log(err)
